c3d24da6e57c70b7569a1923f0aa3cf8ff3033ef
ElliteDeveloper <103085885+ElliteDeveloper@users.noreply.github.com>
2022-05-20 18:58:57 +0900

    Update HLEventBusFragment.kt

--- app/src/main/java/com/horselinc/views/fragments/HLEventBusFragment.kt
+++ app/src/main/java/com/horselinc/views/fragments/HLEventBusFragment.kt
@@ -3,10 +3,6 @@ package com.horselinc.views.fragments
 import android.os.Bundle
 import org.greenrobot.eventbus.EventBus
 
-/**
- *  Created by TengFei Li on 26, August, 2019
- */
-
 open class HLEventBusFragment : HLBaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
